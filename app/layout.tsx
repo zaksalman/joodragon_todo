e1be79648db33d5122ca1342f7729422d5dd1aca
jojozon <jojozon@naver.com>
2024-12-19 00:07:45 +0900

    1212

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "config/material-tailwind-theme-provider";
+import ReactQueryClientProvider from "config/ReactQueryClientProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,20 +13,21 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ThemeProvider>
-      {/* @ts-ignore */}
-      <html lang="en">
-        <head>
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-            integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
-            crossOrigin="anonymous"
-            referrerPolicy="no-referrer"
-          />
-        </head>
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ThemeProvider>
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
+      <ReactQueryClientProvider>
+        <ThemeProvider>
+          <body className={inter.className}>{children}</body>
+        </ThemeProvider>
+      </ReactQueryClientProvider>
+    </html>
   );
 }
